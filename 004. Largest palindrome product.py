83317bdcfda2bdcc06389be98b567204bdb43fe1
kamaramak <108427651+kamaramak@users.noreply.github.com>
2025-02-12 18:06:18 +0300

    Update 004. Largest palindrome product.py

--- 004. Largest palindrome product.py
+++ 004. Largest palindrome product.py
@@ -3,10 +3,14 @@ The largest palindrome made from the product of two 2-digit numbers is 9009 = 91
 Find the largest palindrome made from the product of two 3-digit numbers.'''
 
 
-ans = []
-for i in range(100, 1000):
-    for j in range(100, 1000):
-        n = i * j
-        if str(n) == str(n)[::-1]:
-            ans.append(n)
-print(*sorted(ans), sep='\n')
+def largest_palindrom():
+    ans = 0
+    for num1 in range(999, 99, -1):
+        for num2 in range(999, 99, -1):
+            prod = num1 * num2
+            if str(prod) == str(prod)[::-1]:
+                if prod > ans:
+                    ans = prod
+    return ans
+
+print(largest_palindrom())
